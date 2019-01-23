b2013943c3fe2ad7387ea9733d368660cb410f41
Nishit Jain <nishhit777@gmail.com>
2019-01-23 17:43:39 +0530

    Code file created.

---
+++ linear-regression-basics/code.py
@@ -0,0 +1,61 @@
+# --------------
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import train_test_split
+# code starts here
+df = pd.read_csv(path)
+df.head()
+X = df.drop(['list_price'],1)
+y = df['list_price']
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=6)
+# code ends here
+
+
+
+# --------------
+import matplotlib.pyplot as plt
+
+# code starts here        
+cols = X_train.columns
+fig,axes = plt.subplots(nrows=3,ncols=3)
+for i in range(3):
+    for j in range(3):
+        col = cols[i*3+j]
+        axes[i][j] = plt.scatter(X_train[col],y_train)
+
+# code ends here
+
+
+
+# --------------
+# Code starts here
+corr = X_train.corr()
+print(corr)
+X_train = X_train.drop(['play_star_rating','val_star_rating'],1)
+X_test = X_test.drop(['play_star_rating','val_star_rating'],1)
+# Code ends here
+
+
+# --------------
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Code starts here
+regressor = LinearRegression()
+y_pred = regressor.fit(X_train,y_train).predict(X_test)
+mse = mean_squared_error(y_test,y_pred)
+print(mse)
+r2 = r2_score(y_test,y_pred)
+print(r2)
+# Code ends here
+
+
+# --------------
+# Code starts here
+residual = np.abs(y_pred-y_test)
+residual.hist()
+
+
+# Code ends here
+
+
